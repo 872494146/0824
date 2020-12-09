ac6cd07f3633405d75c717779d70dc70a0621521
Yys888812 <872494146@qq.com>
2020-12-09 09:59:50 +0800

    y

---
+++ src/pages/login/login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class='box'>
+      <div class="con">
+          <h3>登录</h3>
+          <div class="inp"><input type="text"></div>
+          <div class='inp'><input type="text"></div>
+          <div class='inp'><button>登录</button></div>
+      </div>
+  </div>
+</template>
+<style  scoped>
+.box{  
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(to right,#563443,#303D60);
+}
+.con{
+    width: 350px;
+    height: 250px;
+    background: #fff;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    border-radius: 10px;
+}
+.con h3{
+    text-align: center;
+}
+.con .inp{
+    width: 60%;
+    height: 30px;
+    margin: 25px auto;
+}
+.con input{
+    width: 100%;
+    height: 30px;
+}
+.inp button{
+    width: 100%;
+    height: 30px;
+    background-color: #409EFF;
+    color: #fff;
+    outline: none;
+    border: none;
+}
+</style> 
---
+++ src/pages/menu/menu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class='box'>
+    <el-button type="primary" @click="willAdd">添加</el-button>
+    <v-add :info="info" ref="one"></v-add>
+    <v-list @edit="edit($event)"></v-list>
+  </div>
+</template>
+<script>
+import vAdd from "./components/add";
+import vList from "./components/list";
+export default {
+  components: {
+    vAdd,
+    vList,
+  },
+  data() {
+    return {
+      info: {
+        isShow: false,
+        isAdd: true,
+        title: "菜单添加",
+      },
+    };
+  },
+  methods: {
+    willAdd() {
+      this.info.isShow = !this.info.isShow;
+      this.info.isAdd = true;
+      this.info.title = "菜单添加";
+      this.$refs.one.empty();
+    },
+    // 编辑 子list传值过来的数据
+    edit(e) {
+      this.info = {
+        isShow: true,
+        isAdd: false,
+        title: "菜单编辑",
+      };
+      // this.info.isShow = true;
+      this.$refs.one.look(e);
+    },
+  },
+};
+</script>
+<style scoped>
+.box{
+  margin-top:20px
+}
+</style>
